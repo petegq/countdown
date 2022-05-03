de94c51580b332a8516f6fe40990aa31e78432e1
Peter Garside-Quin <petegq@gmail.com>
2022-05-03 21:35:12 +0100

    Set state

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,14 +6,22 @@ import { Header } from '../components/Header'
 import { TimerInput } from '../components/TimerInput'
 
 const Home: NextPage = () => {
+  const [newTime, setNewTime] = useState<number>(0)
+  const [days, setDays] = useState<number>(0);
+  const [hours, setHours] = useState<number>(0);
+  const [minutes, setMinutes] = useState<number>(0);
+  const [seconds, setSeconds] = useState<number>(0);
+  const [message, setMessage] = useState<string>("");
 
-return (
+ const timeToDays = time * 60 * 60 * 24 * 1000;
 
-<div className="flex min-h-screen flex-col items-center bg-[#1e1f29]">
+ let countDownDate = new Date().getTime() + timeToDays;
+
+  return (
+    <div className="flex min-h-screen flex-col items-center bg-[#1e1f29]">
       <Head>
         <title>Launch Countdown Timer</title>
         <link rel="icon" href="/favicon.ico" />
-
       </Head>
 
       <Header message={message} />
@@ -24,11 +32,15 @@ return (
         minutes={minutes}
         seconds={seconds}
       />
-      <TimerInput value={newTime} handleClick={handleClick} handleChange={handleChange} />
+      <TimerInput
+        value={newTime}
+        handleClick={handleClick}
+        handleChange={handleChange}
+      />
 
       <Footer />
     </div>
+  )
+}
 
-)}
-
-export default Home;
+export default Home
