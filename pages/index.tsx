5ffe0b763089d4465166840d819a75320b17aa25
Peter Garside-Quin <petegq@gmail.com>
2022-05-03 21:36:30 +0100

    Add effect

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,15 +7,48 @@ import { TimerInput } from '../components/TimerInput'
 
 const Home: NextPage = () => {
   const [newTime, setNewTime] = useState<number>(0)
-  const [days, setDays] = useState<number>(0);
-  const [hours, setHours] = useState<number>(0);
-  const [minutes, setMinutes] = useState<number>(0);
-  const [seconds, setSeconds] = useState<number>(0);
-  const [message, setMessage] = useState<string>("");
+  const [days, setDays] = useState<number>(0)
+  const [hours, setHours] = useState<number>(0)
+  const [minutes, setMinutes] = useState<number>(0)
+  const [seconds, setSeconds] = useState<number>(0)
+  const [message, setMessage] = useState<string>('')
 
- const timeToDays = time * 60 * 60 * 24 * 1000;
+  const timeToDays = time * 60 * 60 * 24 * 1000
 
- let countDownDate = new Date().getTime() + timeToDays;
+  let countDownDate = new Date().getTime() + timeToDays
+
+  useEffect(() => {
+    var updateTime = setInterval(() => {
+      var now = new Date().getTime()
+
+      var difference = countDownDate - now
+
+      var newDays = Math.floor(difference / (1000 * 60 * 60 * 24))
+      var newHours = Math.floor(
+        (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      )
+      var newMinutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60))
+      var newSeconds = Math.floor((difference % (1000 * 60)) / 1000)
+
+      setDays(newDays)
+      setHours(newHours)
+      setMinutes(newMinutes)
+      setSeconds(newSeconds)
+
+      if (difference <= 0) {
+        clearInterval(updateTime)
+        setMessage('The Launch Has Started')
+        setDays(0)
+        setHours(0)
+        setMinutes(0)
+        setSeconds(0)
+      }
+    })
+
+    return () => {
+      clearInterval(updateTime)
+    }
+  }, [time])
 
   return (
     <div className="flex min-h-screen flex-col items-center bg-[#1e1f29]">
