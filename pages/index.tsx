5447620991793faf1d52298fc8c55c9d8cec36f2
Peter Garside-Quin <petegq@gmail.com>
2022-05-03 21:33:38 +0100

    Scaffold home page components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,34 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import { TimerContainer } from '../components/TimerContainer'
+import { Footer } from '../components/Footer'
+import { Header } from '../components/Header'
+import { TimerInput } from '../components/TimerInput'
 
 const Home: NextPage = () => {
-  return (
-    <div className="flex min-h-screen flex-col items-center bg-[#1e1f29]">
+
+return (
+
+<div className="flex min-h-screen flex-col items-center bg-[#1e1f29]">
       <Head>
         <title>Launch Countdown Timer</title>
         <link rel="icon" href="/favicon.ico" />
-      </Head>
-    </div>
-  )
-}
 
-export default Home
+      </Head>
+
+      <Header message={message} />
+
+      <TimerContainer
+        days={days}
+        hours={hours}
+        minutes={minutes}
+        seconds={seconds}
+      />
+      <TimerInput value={newTime} handleClick={handleClick} handleChange={handleChange} />
+
+      <Footer />
+    </div>
+
+)}
+
+export default Home;
